0ae75f2d53c72f39de1386689d6ff2173c3c8669
= <hardlane2000@gmail.com>
2022-05-03 11:10:13 +0200

    committing before upload

--- static/js/heroes.js
+++ static/js/heroes.js
@@ -61,3 +61,10 @@ Array.from(heroesGridHero).forEach(element => {
 console.log(mostPopularHeroes);
 console.log(highestWinrates);
 console.log(hero_popularities);
+
+var heroGridImages = document.getElementsByClassName("hero_grid_image");
+Array.from(heroGridImages).forEach(element => {
+
+    element.setAttribute("src", `../static/images/hero_avatars/${heroes[Array.from(heroGridImages).indexOf(element)]}`);
+
+})
