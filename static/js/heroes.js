faebbd67572cff39aec6e60a845e881ce6d658b9
= <hardlane2000@gmail.com>
2022-05-03 23:06:33 +0200

    committing before upload

--- static/js/heroes.js
+++ static/js/heroes.js
@@ -9,12 +9,13 @@ heroWinrates.sort(function(a, b) {
 var mostPopularHeroes = hero_popularities.slice(0, 10).map(c => c["hero_name"]);
 var highestWinrates = heroWinrates.slice(0, 10).map(c => c["hero_name"]);
 
-var heroGridImages = document.getElementsByClassName("hero_grid_image");
-Array.from(heroGridImages).forEach(element => {
-
-    element.setAttribute("src", `../static/images/hero_avatars/${heroes[Array.from(heroGridImages).indexOf(element)]}`);
-
-})
+//if (!(typeof(heroes) == 'undefined')) {
+//var heroGridImages = document.getElementsByClassName("hero_grid_image");
+//Array.from(heroGridImages).forEach(element => {
+//
+//    element.setAttribute("src", `../static/images/hero_avatars/${heroes[Array.from(heroGridImages).indexOf(element)]}`);
+//
+//}) }
 
 Array.from(heroesGridHero).forEach(element => {
 
