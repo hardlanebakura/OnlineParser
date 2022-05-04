8a38782af9ef5bdf9c3c3baa3d4456c5460e7ee3
= <hardlane2000@gmail.com>
2022-05-04 08:15:48 +0200

    committing before upload

--- static/js/config.js
+++ static/js/config.js
@@ -16,7 +16,7 @@ function configDotapicker() {
 
         console.log("1");
         //heroImg.setAttribute("src", `${heroes[Array.from(heroImgs).indexOf(heroImg)]}`);
-        console.log(`../static/images/hero_avatars/${{heroes[Array.from(heroImgs).indexOf(heroImg)]}}`);
+        console.log(`../static/images/hero_avatars/${heroes[Array.from(heroImgs).indexOf(heroImg)]}`);
 
     })
 
