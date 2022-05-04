e69485f21cffbf49688e689737056910d1131b48
= <hardlane2000@gmail.com>
2022-05-04 07:31:45 +0200

    committing before upload

--- static/js/config.js
+++ static/js/config.js
@@ -1,13 +1,14 @@
 //make files deployable
 //if (window.location.href)
 var address = window.location.href.split("/")[window.location.href.split("/").length - 2];
-(window.location.href.includes(".com") ? : (address == "dotapicker") ? configDotapicker() : config(); : {};
+(window.location.href.includes(".com")) ? : (address == "dotapicker") ? configDotapicker() : config() : {};
 
 function configDotapicker() {
 
     var heroImgs = document.getElementsByClassName("dotapicker_hero_img");
     Array.from(heroImgs).forEach(heroImg => {
 
+        console.log("1");
         heroImg.setAttribute("src", `${heroes[Array.from(heroImgs).indexOf(heroImg)]}`);
 
     })
