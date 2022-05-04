bef0134af7bbefaf40bf81aaf038c3788413432c
= <hardlane2000@gmail.com>
2022-05-04 06:57:11 +0200

    committing before upload

--- static/js/config.js
+++ static/js/config.js
@@ -1,16 +1,18 @@
 //make files deployable
 //if (window.location.href)
-console.log(window.location.href);
+var address = window.location.href.split("/")[window.location.href.split("/").length - 2];
+(address == "dotapicker") ? configDotapicker() : config();
 
 function configDotapicker() {
 
-
+    var heroImg = document.getElementsByClassName("dotapicker_hero_img");
+    console.log(heroes);
 
 }
 
 function config() {
 
-    
+    console.log("asd");
 
 }
 
