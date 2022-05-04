63367d4abc18dff01b16b653ba5f9e982655c426
= <hardlane2000@gmail.com>
2022-05-04 13:06:17 +0200

    committing before upload

--- static/js/config.js
+++ static/js/config.js
@@ -55,18 +55,10 @@ function configHeroesProduction() {
     console.log(heroes);
     heroes.sort();
 
-    heroes.forEach(hero => {
-
-        heroes[heroes.indexOf(hero)] = hero.split("/hero_avatars/")[1];
-
-    })
-
     var heroImgs = document.getElementsByClassName("hero_grid_image");
     var heroGridTitles = document.getElementsByClassName("hero_grid_title");
     Array.from(heroImgs).forEach(heroImg => {
 
-        console.log(heroes);
-        console.log(heroes[Array.from(heroImgs).indexOf(heroImg)]);
         heroImg.setAttribute("src", `../static/images/hero_avatars/${heroes[Array.from(heroImgs).indexOf(heroImg)]}`);
         console.log(`../static/images/hero_avatars/${heroes[Array.from(heroImgs).indexOf(heroImg)]}`);
         heroGridTitles[Array.from(heroImgs).indexOf(heroImg)].innerText = heroes[Array.from(heroImgs).indexOf(heroImg)].split(".png")[0];
