c5630ebff374f5e32c3215d7ac8d01f8fb3afc23
= <hardlane2000@gmail.com>
2022-05-04 03:26:26 +0200

    committing before upload

--- static/js/heroes.js
+++ static/js/heroes.js
@@ -9,12 +9,6 @@ heroWinrates.sort(function(a, b) {
 var mostPopularHeroes = hero_popularities.slice(0, 10).map(c => c["hero_name"]);
 var highestWinrates = heroWinrates.slice(0, 10).map(c => c["hero_name"]);
 
-//heroes.forEach(hero => {
-//
-//    if (hero.length > 22) hero = hero.split("/hero_avatars/")[1];
-//
-//})
-
 Array.from(heroesGridHero).forEach(element => {
 
     //hero image
