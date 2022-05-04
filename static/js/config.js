e42d41f36f84abc8384237d0c21f3f5f8cfb727e
= <hardlane2000@gmail.com>
2022-05-04 12:29:27 +0200

    committing before upload

--- static/js/config.js
+++ static/js/config.js
@@ -1,7 +1,21 @@
 //make files deployable
 var address = window.location.href.split("/")[window.location.href.split("/").length - 2];
-(window.location.href.includes(".com")) ? (address == "dotapicker") ? configDotapicker() : ? (address == "heroes") ? configHeroesProduction() : configHeroesDevelopment() : (address == "dotapicker") ? config() : {}
-
+(window.location.href.includes(".com")) ?
+(address == "dotapicker") ?
+    configDotapicker()
+    : (address == "heroes") ?
+        configHeroesProduction()
+        : {}
+:(address == "dotapicker") ?
+    config()
+    : {}
+//(address == "dotapicker") ?
+//    configDotapicker()
+//    : {}
+//:(address == "dotapicker") ?
+//    config()
+//    : {}
+//(window.location.href.includes(".com")) ? (address == "dotapicker") ? configDotapicker() : {} : (address == "dotapicker") ? config() : {}
 function configDotapicker() {
 
     heroes.forEach(hero => {
@@ -39,7 +53,7 @@ function config() {
 function configHeroesProduction() {
 
     heroes.sort();
-    
+
     heroes.forEach(hero => {
 
         heroes[heroes.indexOf(hero)] = hero.split("/hero_avatars/")[1];
