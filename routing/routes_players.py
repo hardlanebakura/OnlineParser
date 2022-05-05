c23c49757efd31459d2ffb8ba1100b0537bded55
= <hardlane2000@gmail.com>
2022-05-05 06:34:38 +0200

    committing before upload

--- routing/routes_players.py
+++ routing/routes_players.py
@@ -3,9 +3,12 @@ from jinja2 import TemplateNotFound
 from log_config import logging
 from player_info import *
 from find_player import *
+from types import SimpleNamespace
 
 players_pages = Blueprint('players', __name__,
                         template_folder='Templates', static_folder='static', url_prefix="/players")
+
+PLAYERS = [34603321]
 
 def search():
     if request.method == "POST":
@@ -20,7 +23,15 @@ def players():
 @players_pages.route("/<int:player_id>")
 def player(player_id):
     search()
-    player = find_player(player_id)
-    player_dict = player.__dict__
-
-    return render_template("player.html", player = player, player_dict = player_dict)
+    if player_id not in PLAYERS:
+        player = find_player(player_id)
+        player_dict = player.__dict__
+        logging.info(player_dict)
+        return render_template("player.html", player = player, player_dict = player_dict)
+    else:
+        player = DatabaseAtlas.find("dota_players", {"id": player_id})
+        return render_template("player.html", player = SimpleNamespace(**player), player_dict = player)
+    #player = find_player(player_id)
+    #player_dict = player.__dict__
+    #logging.info(player_dict)
+    #DatabaseAtlas.insertOne("dota_players", player.__dict__)
